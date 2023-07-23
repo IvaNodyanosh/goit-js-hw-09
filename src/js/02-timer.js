617ddd69790f70c9c3b5fbe89b77926d8cdd656c
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-07-23 20:39:11 +0300

    completed task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,93 @@
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+const startBtn = document.querySelector('[data-start]');
+const datDays = document.querySelector('[data-days]');
+const datHours = document.querySelector('[data-hours]');
+const datMinutes = document.querySelector('[data-minutes]');
+const datSeconds = document.querySelector('[data-seconds]')
+
+startBtn.disabled = true;
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
+let userDate = null;
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      
+    userDate = selectedDates[0];    
+    
+    if (userDate > Date.now()) {
+        
+        startBtn.disabled = false;
+
+    } else {
+        alert("Please choose a date in the future");
+
+        startBtn.disabled = true;
+    }
+  },
+};
+
+const input = document.querySelector('#datetime-picker');
+
+
+
+flatpickr(input, options);
+
+
+
+const startTimer = function () {
+    const dataId = setInterval(() => {
+
+            const time = userDate - new Date;
+
+
+    const convertTime = convertMs(time);
+
+  
+        if (time > 0) {
+    
+
+            datDays.textContent = convertTime.days.toString().padStart(2, '0');
+            datHours.textContent = convertTime.hours.toString().padStart(2, '0');
+            datMinutes.textContent = convertTime.minutes.toString().padStart(2, '0');
+            datSeconds.textContent = convertTime.seconds.toString().padStart(2, '0');
+        } else {
+            clearInterval(dataId);
+            alert("job done time is up")
+        }
+    }, 1000)
+}
+
+startBtn.addEventListener('click', startTimer);
